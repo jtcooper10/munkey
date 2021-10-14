845e4e1dec3ccdb07c7c3acf2ec019830229c527
Josh C <jcooper6@unca.edu>
2021-10-13 21:40:01 -0400

    Initial commit

---
+++ munkey/munkey.ts
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2021
+ *
+ * MIT License (MIT)
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the “Software”), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies
+ * or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @author  : Joshua Cooper
+ * @created : 10/13/2021
+ */
+
+const express = require("express");
+
+async function configureRoutes(app: typeof express): Promise<typeof express>  {
+    app.get("/", function(request, response) {
+        response.send("Hello, world!\n");
+    });
+
+    return app;
+}
+
+if (require.main === module) {
+    configureRoutes(express())
+        .then(app => {
+            app.listen(8000, () => {
+                console.log("Listening on port 8000");
+            });
+        });
+}
