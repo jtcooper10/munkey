d00b38f53c0665f06abc6e83ecd7a6c67e2a1a23
Joshua Cooper <josh@joshdotc.com>
2022-02-03 18:30:43 -0500

    New `VaultNetwork` service in protocol buffer
    - Add `--rpc` command-line arg to set RPC port

--- munkey/server/pipe/PipeCommandServer.ts
+++ munkey/server/pipe/PipeCommandServer.ts
@@ -3,14 +3,20 @@ import * as grpc from "@grpc/grpc-js";
 import CommandServer from "../CommandServer";
 import { ServiceContainer } from "../../services";
 import {
+    IVaultNetworkServer,
+    IVaultNetworkService,
     IVaultServer,
     IVaultService,
+    VaultNetworkService,
     VaultService,
 } from "@munkey/munkey-rpc";
-import createVaultServer from "./rpc";
+import createVaultServer, {
+    createVaultNetworkServer
+} from "./rpc";
 
 interface RpcContainer {
     vaultService: IVaultService;
+    vaultNetworkService: IVaultNetworkService;
 }
 
 
@@ -21,12 +27,14 @@ export default class PipeCommandServer extends CommandServer {
         super(services);
         this.rpc = rpc ?? {
             vaultService: VaultService,
+            vaultNetworkService: VaultNetworkService,
         };
     }
 
     private static bindService(server: grpc.Server, credentials: grpc.ServerCredentials, address: string): Promise<grpc.Server> {
         return new Promise<grpc.Server>((resolve, reject) => {
             server.bindAsync(address, credentials, err => {
+                console.log(err);
                 if (err)
                     reject(err);
                 server.start();
@@ -35,15 +43,33 @@ export default class PipeCommandServer extends CommandServer {
         });
     }
 
+    /**
+     * @name addServices
+     * @private
+     * @method
+     * Attach known gRPC services to the given server.
+     * If any new gRPC services are creates, they should be configured here.
+     * 
+     * @param server gRPC server to attach known services to.
+     */
+    private addServices(server: grpc.Server): void {
+        server.addService(this.rpc.vaultService, this.vaultServer);
+        server.addService(this.rpc.vaultNetworkService, this.vaultNetworkServer);
+    }
+
     private get vaultServer(): IVaultServer {
         // The command server itself cannot implement the service methods itself.
         // This is because the `CommandServer` class has its own properties/methods,
         // which might accidentally get called by RPC (thus the UntypedHandleCall indexer).
         return createVaultServer(this);
     }
+    
+    private get vaultNetworkServer(): IVaultNetworkServer {
+        return createVaultNetworkServer(this);
+    }
 
-    public async useGrpc(server: grpc.Server, address: string = "127.0.0.1:8000"): Promise<grpc.Server> {
-        server.addService(this.rpc.vaultService, this.vaultServer);
+    public async useGrpc(server: grpc.Server, address: string = "127.0.0.1:5050"): Promise<grpc.Server> {
+        this.addServices(server);
         await PipeCommandServer.bindService(server, grpc.ServerCredentials.createInsecure(), address);
         return server;
     }
